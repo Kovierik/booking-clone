d0e9325738a25a8399b1464e9159bf4044ffb93b
Kovács Erik <kovi.erik98@gmail.com>
2024-01-02 20:36:15 +0100

    creating the search page template

---
+++ app/search/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+// import { fetchResults } from "@/lib/fetchResults";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+
+type Props = {
+  searchParams: SearchParams;
+};
+
+export type SearchParams = {
+  url: URL;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+  checkin: string;
+  checkout: string;
+};
+
+async function SearchPage({ searchParams }: Props) {
+  if (!searchParams.url) return notFound();
+
+  const results = await fetchResults(searchParams);
+
+  if (!results) return <div>No results...</div>;
+
+  return <section>Results</section>;
+}
+
+export default SearchPage;
